61eec2a1073f4ffff0a4994abfe7cad6a690245e
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-02-14 21:12:48 -0300

    Tela com circulo criada

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -1,3 +1,17 @@
 import pygame
 
 pygame.init()
+
+AMARELO = (255, 255, 0)
+tela = pygame.display.set_mode((640, 480), 0)
+
+while True:
+    # calcular regras
+    # pintar a tela
+    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 0)
+    pygame.display.update()
+
+    # Analisar eventos
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
